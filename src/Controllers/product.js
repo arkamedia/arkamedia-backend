3c62d3e5afa75e2808abc6f30b608c2b4123230e
IyanSR <ikhyan7@gmail.com>
2019-11-19 20:57:58 +0700

    Upload Product Image

--- src/Controllers/product.js
+++ src/Controllers/product.js
@@ -1,5 +1,7 @@
 const productModel = require('../Models/product');
 const moment = require('moment');
+const upload = require('../Configs/multer');
+const cloudinary = require('../Configs/cloudinary');
 
 module.exports = {
 	// @Method GET
@@ -24,42 +26,62 @@ module.exports = {
 	},
 	// @Method POST
 	insertProduct: (req, res) => {
-		const id = Math.floor(Math.random() * 99990) + 99;
-		const date = moment().format('YYYY-MM-DD');
+		upload.single('imgurl')(req, res, async err => {
+			if (err) {
+				res.status(404).json({ msg: err });
+			} else {
+				if (req.file == undefined) {
+					res.status(404).json({ msg: 'No File Selected' });
+				} else {
+					try {
+						cloudinary.uploader
+							.upload(req.file.path, { folder: 'arkamedia' })
+							.then(result => {
+								const id = Math.floor(Math.random() * 99990) + 99;
+								const date = moment().format('YYYY-MM-DD');
 
-		const product = {
-			product_id: id,
-			detail_id: id,
-			product_name: req.body.product_name,
-			imgurl: req.body.imgurl,
-			cat_name: req.body.cat_name,
-			quantity: req.body.quantity,
-			price: req.body.price,
-			description: req.body.description,
-		};
-		const detail = {
-			detail_id: id,
-			weight: req.body.weight,
-			width: req.body.width,
-			height: req.body.height,
-			ISBN: req.body.ISBN,
-			publisher: req.body.publisher,
-			pages: req.body.pages,
-			genre: req.body.genre,
-			author: req.body.author,
-			import: req.body.import,
-			publish_date: date,
-			pages: req.body.pages,
-		};
-		productModel
-			.insertProduct(product, detail)
-			.then(result => {
-				console.log(result);
-				res.status(200).json({
-					msg: 'Succes Add Product',
-					result: [product, detail],
-				});
-			})
-			.catch(err => res.json(err));
+								const product = {
+									product_id: id,
+									detail_id: id,
+									product_name: req.body.product_name,
+									imgurl: result.url,
+									cat_name: req.body.cat_name,
+									quantity: req.body.quantity,
+									price: req.body.price,
+									description: req.body.description,
+								};
+								const detail = {
+									detail_id: id,
+									weight: req.body.weight,
+									width: req.body.width,
+									height: req.body.height,
+									ISBN: req.body.ISBN,
+									publisher: req.body.publisher,
+									pages: req.body.pages,
+									genre: req.body.genre,
+									author: req.body.author,
+									import: req.body.import,
+									publish_date: date,
+									pages: req.body.pages,
+								};
+								productModel
+									.insertProduct(product, detail)
+									.then(result => {
+										console.log(result);
+										res.status(200).json({
+											msg: 'Succes Add Product',
+											result: [product, detail],
+										});
+									})
+									.catch(err => res.json(err));
+							});
+					} catch (err) {
+						res.json({
+							err: 'Cannot Upload File',
+						});
+					}
+				}
+			}
+		});
 	},
 };
